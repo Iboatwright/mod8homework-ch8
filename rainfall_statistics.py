02060268b88cb310c327acd6e26e0852282bc6f0
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-03-10 11:17:41 -0500

    Initial commit.  Pseudocode complete.

---
+++ rainfall_statistics.py
@@ -0,0 +1,11 @@
+# rainfall_statistics.py
+# Exercise selected: Chapter 8 program 3
+# Name of program: Rainfall Statistics
+# Description of program: This program allows the user to enter the total
+#   rainfall for each month.  It calculates the total rainfall
+#   for the year, the average monthly rainfall, and the months with the
+#   highest and lowest amounts.  Then it displays the results.
+#
+# Ivan Boatwright
+# March 10, 2016
+
